fb9ec4c406dce2a11d11510e96b47ca15c8ab115
MalachyCode <nwaformalachy2@gmail.com>
2023-09-28 19:58:18 +0100

    Wrap the app inside a browser rouser imported as router to enable the use of react routers1~

--- bloglist-frontend-redux/src/index.js
+++ bloglist-frontend-redux/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 // import { setBlogs } from './reducers/blogReducer';
 // import blogService from './services/blogs';
 
@@ -11,7 +12,9 @@ import store from './store';
 // blogService.getAll().then((blogs) => store.dispatch(setBlogs(blogs)));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>
 );
