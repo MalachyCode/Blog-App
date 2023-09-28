c6acc819fced5fd74b7c28c766a1979d72895203
MalachyCode <nwaformalachy2@gmail.com>
2023-09-28 19:56:43 +0100

    Component used to hide and show forms for adding new bogs and user with the click of a button

--- bloglist-frontend-redux/src/components/Togglling.js
+++ bloglist-frontend-redux/src/components/Togglling.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Button } from 'react-bootstrap';
 
 const Togglling = (props) => {
   const [visible, setVisible] = useState(false);
@@ -12,13 +13,23 @@ const Togglling = (props) => {
   };
 
   return (
-    <div>
+    <div className='toggling'>
       <div style={hideWhenVisible}>
-        <button onClick={toggleVisibility}>{props.buttonLabel}</button>
+        {/* <button onClick={toggleVisibility}>{props.buttonLabel}</button> */}
+        <Button
+          variant='outline-dark'
+          size='sm'
+          type='submit'
+          onClick={toggleVisibility}
+        >
+          {props.buttonLabel}
+        </Button>
       </div>
       <div style={showWhenVisible}>
         {props.children}
-        <button onClick={toggleVisibility}>cancel</button>
+        <Button variant='outline-dark' size='sm' onClick={toggleVisibility}>
+          cancel
+        </Button>
       </div>
     </div>
   );
